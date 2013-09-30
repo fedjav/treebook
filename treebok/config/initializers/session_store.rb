4ba670dd4238134286e9528e4b6e081aa5b4d7a0
Federico Duran <fede_duran77@yahoo.com.mx>
2013-09-30 13:14:06 -0500

    Created a rails applications and added status updates

---
+++ treebok/config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Treebok::Application.config.session_store :cookie_store, key: '_treebok_session'
---
+++ treebook/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Treebook::Application.config.secret_token = '9976e20f9d22e1fa66a7182f2fcd7c1f0f12f9616f404b50e50a1adf38190866e83425efccf2f9d0bc3fc9fe188937497bbdc29b41493c149abc3edce1eaeda8'
